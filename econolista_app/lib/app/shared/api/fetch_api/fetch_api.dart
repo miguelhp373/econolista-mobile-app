7b2f1df52a965b6a70530bd86916ff8748941a8c
Miguel Henrique <miguelhp373@gmail.com>
2024-02-10 19:00:56 -0300

    feat - Mercado Livre API Integration

--- econolista_app/lib/app/shared/api/fetch_api/fetch_api.dart
+++ econolista_app/lib/app/shared/api/fetch_api/fetch_api.dart
@@ -1,29 +1,27 @@
-// ignore_for_file: avoid_print, use_build_context_synchronously
-
-import 'package:http/http.dart' as http;
+import 'package:econolista_app/app/shared/api/fetch_api/fetch_api_versions/blue_cosmos_fetch_api.dart';
+import 'package:econolista_app/app/shared/api/fetch_api/fetch_api_versions/mercado_livre_fetch_api.dart';
 import 'dart:convert' as convert;
 
 import 'package:econolista_app/app/shared/models/product_models.dart';
+import 'package:http/http.dart';
 
 class FetchAPI {
   ///////////////////////////////////////////////////
 
   Future<ProductModels> fetchDataWithBarcode(String barcodeGTIN) async {
     /////////////////////////////////////////////////////////////////
-    var url = Uri.https(
-      const String.fromEnvironment('API_END_POINT'),
-      '/gtins/$barcodeGTIN',
-    );
 
-    var headers = {
-      'X-Cosmos-Token': const String.fromEnvironment('API_KEY_COSMOS_BLUESOFT')
-    };
+    var apiVersion = const String.fromEnvironment('API_VERSION');
 
-    var response = await http.get(url, headers: headers);
+    Response response = (apiVersion == 'V2'
+        ? await BlueCosmosFetchAPI().fetchDataWithBarcode(barcodeGTIN)
+        : await MercadoLivreFetchAPI().fetchDataWithBarcode(barcodeGTIN));
 
     if (response.statusCode == 200) {
       var jsonResponse =
           convert.jsonDecode(response.body) as Map<String, dynamic>;
+
+      //print(jsonResponse);
 
       return ProductModels.fromJson(jsonResponse);
     }
---
+++ econolista_app/lib/app/shared/api/fetch_api/fetch_api_versions/mercado_livre_fetch_api.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: avoid_print, use_build_context_synchronously
+
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+class MercadoLivreFetchAPI {
+  ///////////////////////////////////////////////////
+  //https://api.mercadolibre.com/products/search?status=active&site_id=MLB&product_identifier=4005900707536&category_id=MLB263832
+  Future<Response> fetchDataWithBarcode(String barcodeGTIN) async {
+    var url = Uri.https(
+      const String.fromEnvironment('API_END_POINT_V2'),
+      '/products/search',
+      {
+        'status': 'active',
+        'site_id': 'MLB',
+        'product_identifier': barcodeGTIN,
+        'category_id': 'MLB263832', //supermarket products
+      },
+    );
+
+    var headers = {'Authorization': const String.fromEnvironment('API_KEY_V2')};
+
+    var response = await http.get(url, headers: headers);
+
+    if (response.statusCode == 200) {
+      var jsonResponse = json.decode(response.body) as Map<String, dynamic>;
+      var results = jsonResponse['results'] as List<dynamic>;
+
+      if (results.isNotEmpty) {
+        var firstResult = results.first;
+        var productName = firstResult['name'];
+        var productPhotoUrl = firstResult['pictures'][0]['url'];
+        var productBarcode = jsonResponse['product_identifier'];
+
+        // Cria um novo mapa com os dados relevantes
+        var responseData = {
+          'description': productName,
+          'gtin': productBarcode,
+          'thumbnail': productPhotoUrl,
+          // Adicione outros campos relevantes conforme necessário
+        };
+
+        // Converte o mapa para uma string JSON
+        var responseBody = json.encode(responseData);
+
+        // Cria um novo objeto Response com o corpo JSON
+        var newResponse = Response(responseBody, response.statusCode);
+
+        return newResponse;
+      }
+    }
+
+    return response; // Retorna a resposta original se não houver resultados ou se houver erro
+  }
+}
